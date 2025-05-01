56bda6d6f1c87b50f96f15b9cc00fa4d158f5a10
Impractical9 <jeisson20247@gmail.com>
2025-04-30 20:45:36 -0500

    algunos cambios nuevos

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 #require() establece que el código del archivo invocado es requerido, es decir, obligatorio para el funcionamiento del programa. Por ello, si el archivo especificado en la función require() no se encuentra saltará un error “PHP Fatal error” y el programa PHP se detendrá.
 
 #La versión require_once() funcionan de la misma forma que sus respectivo, salvo que, al utilizar la versión _once, se impide la carga de un mismo archivo más de una vez.
-#Si requerimos el mismo código más de una vez corremos el riesgo de redeclaraciones de variables, funciones o clases. 
+#Si requerimos el mismo código más de una vez corremos el riesgo de redeclaraciones de variables funcione y clases 
 
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/registro.controlador.php";
