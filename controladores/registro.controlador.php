9dbae6ef62b003d28efaf7c01dd9d1169bb9dc91
Impractical9 <jeisson20247@gmail.com>
2025-05-05 19:56:50 -0500

    registro

--- controladores/registro.controlador.php
+++ controladores/registro.controlador.php
@@ -25,3 +25,49 @@ class ControladorRegistro{
     }
  
 }
+
+/*=============================================
+    Ingresar Usuario
+    =============================================*/
+
+    public function ctrIngreso(){
+
+        if(isset ($_POST["ingresoCorreo"])){
+
+            $tabla = "registro";
+            $item = "correo";
+            $valor = $_POST["ingresoEmail"];
+
+            $respuesta = ModeloRegistro::mdlSeleccionarRegistro($tabla, $item, $valor);
+
+            if($respuesta["correo"] == $_POST["ingresoCorreo"] && $respuesta["clave"] == $_POST["ingresoClave"]){ 
+
+                $_SESSION["validarIngreso"] = "ok";
+
+                echo '<script>
+
+                if ( window.history.replaceState ) {
+                    window.history.replaceState( null, null, window.location.href );
+                }
+
+                    window.location = "index.php?pagina=inicio";
+
+                </script>';
+
+            } else {
+
+                echo '<script>
+
+                if ( window.history.replaceState ) {
+                    window.history.replaceState( null, null, window.location.href );
+                }
+
+                </script>';
+
+                echo '<div class="alert alert-success">la contraseña no es valida</div>';
+            }
+
+
+        }
+
+    }
